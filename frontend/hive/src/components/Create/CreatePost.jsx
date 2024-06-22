135329567a2c954bff30ad815eff11a5b0bd1407
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-06-22 21:26:30 +0530

    Add component and pages file

---
+++ frontend/hive/src/components/Create/CreatePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreatePost() {
+  return (
+    <div>Create the post from here</div>
+  )
+}
+
+export default CreatePost
---
+++ frontend/hive/src/pages/Content/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div>Content will be shown here!</div>
+  )
+}
+
+export default Content
