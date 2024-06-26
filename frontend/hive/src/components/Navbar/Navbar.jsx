25485988fdfc277fabccc696b02786fb9cb4ff75
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-06-26 12:23:40 +0530

    Navbar responsive

--- frontend/hive/src/components/Navbar/Navbar.jsx
+++ frontend/hive/src/components/Navbar/Navbar.jsx
@@ -28,15 +28,11 @@ function Navbar() {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
+        <li><a><IoEarth className='size-[15px]' /> Explore</a></li>
         <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
+          <a><TbMessage2Heart className='size-[15px]' /> Messages</a>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a><RiNotification4Line className='size-[15px]'/> Notifications</a></li>
       </ul>
     </div>
     
