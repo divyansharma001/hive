859e597224b2cb9d382e24c3087345616ba40dbb
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-06-29 21:05:27 +0530

    User card completed

--- frontend/hive/src/components/UserCard/UserCard.jsx
+++ frontend/hive/src/components/UserCard/UserCard.jsx
@@ -8,21 +8,37 @@ function UserCard() {
     <div className="card bg-[rgb(15,16,18)] w-80 shadow-xl ml-12 text-white">
   <figure className="px-10 pt-10">
     <img
-      src="Dp.jpg"
+      src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
       alt="Profile Pic"
       className="rounded-full size-24" />
   </figure>
   <div className="card-body items-center text-center">
     <h2 className="text-xl font-medium -mt-6">Divyansh Sharma</h2>
-    <p className='text-[#4b5359] text-sm'>@divyansharma001</p>
+    <p className='text-[#a7acaf] text-sm pb-1'>@divyansharma001</p>
     <div className=' inline-flex'>
-     <div className='text-[#4b5359] pt-1'> <CiLocationOn /></div>
-     <div className='text-[#4b5359] px-1'>Delhi, India</div>
+     <div className='text-[#a7acaf] pt-1'> <CiLocationOn /></div>
+     <div className='text-[#a7acaf] px-1'>Delhi, India</div>
      </div>
-    <p>Don't judge by this. This is still under making.</p>
-    <div className="card-actions">
-      <button className="btn btn-primary">View Profile</button>
+    <p className='text-[#a7acaf] pb-3'>Don't judge by this. This is still under making.</p>
+   
+   
+   
+   <div className='flex'>
+    <div className='text-[#a7acaf] px-3 pt-2'>
+     Posts
+     <div className='text-white'>5</div>
     </div>
+    <div className='text-[#a7acaf] px-3 pt-2'>
+     Followers
+     <div className='text-white'>27</div>
+    </div>
+    <div className='text-[#a7acaf] px-3 pt-2'>
+     Following
+     <div className='text-white'>18</div>
+    </div>
+   </div>
+  
+  
   </div>
 </div>
     </div>
