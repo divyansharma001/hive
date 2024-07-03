6b36e3285e84c4b5babbe766adfcc2d20016f134
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-04 01:58:37 +0530

    Login and signup route

--- frontend/hive/src/main.jsx
+++ frontend/hive/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Navbar from './components/Navbar/Navbar.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -10,11 +10,15 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage.jsx'
+import Login from './pages/Login/Login.jsx'
+import Signup from './pages/Signup/Signup.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
       <Route path='' element={<Homepage/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='signup' element={<Signup/>}/>
     </Route>
   )
 )
