7b09bbc70189146d4535d5c483659f49995c6680
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-07 02:43:41 +0530

    login page frontend completed

--- frontend/hive/src/pages/Login/Login.jsx
+++ frontend/hive/src/pages/Login/Login.jsx
@@ -1,18 +1,91 @@
-import React from 'react'
+import React from "react";
+import { GiTreeBeehive } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
     <>
-    <div className='grid-cols-2'>
-     <div>
-      Photo
-     </div>
-     <div>
-        form
-     </div>
-     </div>
+      <div className="relative py-16 bg-black">
+        <div className="relative container m-auto px-6 text-[#FFDB00] md:px-12 xl:px-40">
+          <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
+            <div className="rounded-xl bg-black shadow-xl">
+              <div className="p-6 sm:p-16">
+                <div className="space-y-4">
+                  <h2 className="mb-8 text-2xl text-[#FFDB00] font-bold">
+                    Login in to unlock the <br /> best of hive.
+                  </h2>
+                </div>
+                <div className="mt-14 grid space-y-4">
+                  <label className="input input-bordered border-[#FFDB00] flex items-center gap-2 bg-black">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70"
+                    >
+                      <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                      <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input type="text" className="grow" placeholder="Email" />
+                  </label>
+
+                  <label className="input input-bordered border-[#FFDB00] flex items-center gap-2 bg-black">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70"
+                    >
+                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input
+                      type="text"
+                      className="grow"
+                      placeholder="Username"
+                    />
+                  </label>
+
+                  <label className="input input-bordered border-[#FFDB00] bg-black flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="h-4 w-4 opacity-70"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <input type="password" className="grow" value="password" />
+                  </label>
+
+
+                  <button className="btn btn-ghost text-xl font-light">Login</button>
+                </div>
+
+                <div className="mt-32 space-y-4 text-gray-600 text-center sm:-mb-8">
+                  <p className="text-sm">
+                    First time on hive?{" "}
+                    <Link to={"/signup"} className="underline">
+                      Register here
+                    </Link>{" "}
+                    and confirm you have read our{" "}
+                    <a href="#" className="underline">
+                      Privacy and Cookie Statement
+                    </a>
+                    
+                  </p>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
