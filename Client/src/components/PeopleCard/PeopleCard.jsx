967933de9d170b864c30e3cd5b245b3439b860f2
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-30 19:27:21 +0530

    Complete redux of get other user

--- Client/src/components/PeopleCard/PeopleCard.jsx
+++ Client/src/components/PeopleCard/PeopleCard.jsx
@@ -2,67 +2,38 @@ import React from "react";
 import { useSelector } from "react-redux";
 import useGetOtherUsers from "../../hooks/useGetOtherUsers";
 
-const details = [
-  {
-    profilePic:
-      "https://vectorseek.com/wp-content/uploads/2023/02/OpenAI-Logo-Vector.jpg",
-    name: "Open Ai",
-    userName: "@OpenAi",
-  },
-  {
-    profilePic:
-      "https://cdn.vox-cdn.com/thumbor/nBEbhF6OktWUWqR3cCmuRNPgOD8=/0x0:179x167/1200x0/filters:focal(0x0:179x167)/cdn.vox-cdn.com/uploads/chorus_asset/file/4019454/googlelogo.0.jpg",
-    name: "Google",
-    userName: "@Google",
-  },
-  {
-    profilePic:
-      "https://www.freepnglogos.com/uploads/twitter-x-logo-png/twitter-x-logo-png-9.png",
-    name: "Twitter",
-    userName: "@Twitter",
-  },
-];
-
 function PeopleCard() {
+  const { user, otherUsers } = useSelector((store) => store.user);
+  useGetOtherUsers(user?.id);
 
-  const {user} = useSelector(store=>store.user);
-  useGetOtherUsers(user?.id)
+  console.log('other', otherUsers);
 
   return (
-    <>
-      <div className="card bg-[rgb(15,16,18)] w-80 shadow-xl ml-12 text-white">
-        <div className="card-body  ">
-          <h2 className="font-semibold text-xl items-center text-center">
-            Who to follow
-          </h2>
+    <div className="card bg-[rgb(15,16,18)] w-80 shadow-xl ml-12 text-white">
+      <div className="card-body">
+        <h2 className="font-semibold text-xl text-center">Who to follow</h2>
 
-          {details.map((detail, index) => (
-            <div className="flex pt-2" key={index}>
-              <div>
-                <img
-                  src={detail.profilePic}
-                  alt=""
-                  className="w-14 h-14 rounded-full"
-                />
-              </div>
-
-              <div>
-                <div className="font-semibold ml-3">{detail.name}</div>
-                <div className="text-[#a7acaf] pl-3 text-sm">
-                  {detail.userName}
-                </div>
-              </div>
-
-              <div className="mt-1 ml-6 ">
-                <button className="btn btn-outline text-[#FFDB00] w-24">
-                  FOLLOW
-                </button>
-              </div>
+        {otherUsers?.map((detail, index) => (
+          <div
+            className="flex items-center py-2 space-x-4"
+            key={index}
+          >
+            <img
+              src={detail?.profilePic || "https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.webp"}
+              alt="Profile"
+              className="w-14 h-14 rounded-full"
+            />
+            <div className="flex-1 min-w-0">
+              <div className="font-semibold truncate">{detail?.name}</div>
+              <div className="text-[#a7acaf] text-sm truncate">@{detail?.username}</div>
             </div>
-          ))}
-        </div>
+            <button className="btn btn-outline text-[#FFDB00] w-24 h-8 flex-shrink-0">
+              FOLLOW
+            </button>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
