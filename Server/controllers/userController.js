28f9deda73e05adcc5e6bf6dd9a9b7e5e4f985af
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-08-14 22:02:36 +0530

    Deploy err

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -81,7 +81,9 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-    }); // 7 days
+      sameSite: "None",
+      secure: process.env.NODE_ENV === "production"
+    }); 
     return res
       .status(200)
       .json({ message: `Welcome back ${user.name}`, user , success: true });
